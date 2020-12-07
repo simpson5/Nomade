7dec4469fdd052bf31f37ee317eab52add051253
simpson <450054@naver.com>
2020-12-08 01:18:21 +0900

    test

---
+++ todo.js
@@ -0,0 +1,9 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = form.querySelector("input"),
+    toDoList = document.queryDrlrvyot(".js-toDoList");
+
+function init(){
+
+}
+
+init();
