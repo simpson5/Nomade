45ed1ffbfc5b3ce307f1d8a60e7d3d4fff82ba38
simpson <450054@naver.com>
2020-12-09 05:02:27 +0900

    Add_bhImageRandom

---
+++ bg.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 4;
+
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src = `image/${imgNumber+1}.jpg`;
+    image.classList.add("bgImage");
+    body.appendChild(image);
+}
+
+//math.floor/ceiling 버림 올림
+//math.random
+function genRandom(){
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init(){
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+
+init();
